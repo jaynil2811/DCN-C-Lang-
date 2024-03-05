ed1ec50bf5bb0bfcfd75a828971d27f0f3892b28
jaynil2811 <142003757+jaynil2811@users.noreply.github.com>
2024-03-05 15:29:34 +0530

    Add files via upload

---
+++ Q1Client.java
@@ -0,0 +1,26 @@
+import java.net.*;
+import java.io.*;
+
+/*
+  Name : JAYNIL PRAJAPATI 
+  ROLL : 3059
+ */
+public class Q1Client {
+    public static void main(String[] args) throws Exception {
+        Socket s = new Socket("localhost", 3333);
+        DataInputStream din = new DataInputStream(s.getInputStream());
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+
+        System.out.println("Enter the Number :");
+        String str = br.readLine();
+        dout.writeUTF(str);
+        dout.flush();
+        String str2 = din.readUTF();
+        System.out.println("Server says Square of : " + str + " is " + str2);
+
+        dout.close();
+        s.close();
+    }
+}
